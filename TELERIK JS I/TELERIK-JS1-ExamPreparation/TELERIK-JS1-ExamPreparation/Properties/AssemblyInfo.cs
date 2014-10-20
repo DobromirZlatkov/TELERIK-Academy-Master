6307935b4b6749784c714e1754580e47131306a3
Dobri <dobromir.v.zlatkov@gmail.com>
2014-10-20 14:47:24 +0300

    JavaScript I

---
+++ TELERIK JS I/TELERIK-JS1-ExamPreparation/TELERIK-JS1-ExamPreparation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TELERIK_JS1_ExamPreparation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TELERIK_JS1_ExamPreparation")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c2b2674-2a92-4f4b-bfa1-a78993a5810c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TELERIK JS I/TELERIK-KPK-HW4/Task1/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Task1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Size
+    {
+         private double width;
+         private double  heigth;
+
+
+         public Size(double inputWidth, double inputHeigth)
+         {
+        
+            this.width = inputWidth;
+            this.heigth = inputHeigth;
+         }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value <= 0)
+	            {
+		            throw new IndexOutOfRangeException();
+	            }
+                else
+	            {
+                    this.width = value;
+	            }
+            }
+        }
+
+        public double Heigth
+        {
+            get
+            {
+                return this.heigth;
+            }
+            set
+            {
+                if (value <= 0)
+	            {
+		            throw new IndexOutOfRangeException();
+	            }
+                else
+	            {
+                    this.heigth = value;
+	            }
+            }
+        }
+    }
+
+    public static Size GetRotatedSize(Size s, double angleOfTheFigureThatWillBeRotaed)
+    {
+        Size papaz = new Size
+        
+      //return new Size(
+      //  Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.wIdTh + 
+      //    Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.Viso4ina,
+      //  Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.wIdTh + 
+      //    Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.Viso4ina);
+    }
+
+}
